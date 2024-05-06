8218ee776777e83b4efa2a685afed35b2d194d6c
wayly1111 <wayly00@mail.ru>
2024-05-06 17:13:27 +0300

    finished game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 
 clear = lambda: os.system("cls")
 
@@ -8,7 +10,9 @@ input('*нажми enter чтобы продолжить*')
 clear()
 print("Поехали!")
 
-word = "слон"
+words = ['пирожок', 'чебурек', 'огурец', 'сосиска', 'котик', 'квокка', 'корабль', 'самолет', 'автомобиль', 'дирижабль']
+# word = words[random.randint(0, len(words) - 1)]
+word = random.choice(words)
 letters = []
 
 win = True
@@ -16,8 +20,6 @@ hp = 10
 
 while hp > 0:
     win = True
-    letter = input("Введите букву: ")
-    letters.append(letter)
     for symb in word:
         if symb in letters:
             print(symb, end=" ")
@@ -30,6 +32,13 @@ while hp > 0:
         print("Молодец ты выйграл")
         break
 
+    letter = input("Введите букву: ")
+    letters.append(letter)
+    clear()
+
     if letter not in word:
         hp -= 1
-        print(f"Осталось попыток {hp}")
+    print(f"Осталось попыток {hp}")
+
+if hp == 0:
+    print("Ты проиграл")
